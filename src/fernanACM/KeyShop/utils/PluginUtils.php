1c888f3fdf09be77e154ed1f90c495d90cf6eed8
DragonfireHD98 <156013157+DragonfireHD98@users.noreply.github.com>
2024-02-26 16:30:20 +0300

    Update PluginUtils.php

--- src/fernanACM/KeyShop/utils/PluginUtils.php
+++ src/fernanACM/KeyShop/utils/PluginUtils.php
@@ -1,13 +1,6 @@
 <?php
     
-#      _       ____   __  __ 
-#     / \     / ___| |  \/  |
-#    / _ \   | |     | |\/| |
-#   / ___ \  | |___  | |  | |
-#  /_/   \_\  \____| |_|  |_|
-# The creator of this plugin was fernanACM.
-# https://github.com/fernanACM
-    
+# CrateKeyShop
 declare(strict_types=1);
 
 namespace fernanACM\KeyShop\utils;
